f63b25d0a63d9dbbd65345804c84e86ac4400bba
Lucas <lucas.vanstaden@enjo.com>
2021-06-03 01:47:41 +0800

    Add in a license file

--- app/code/community/ProxiBlue/UpdateDownloadLinks/Model/Observer.php
+++ app/code/community/ProxiBlue/UpdateDownloadLinks/Model/Observer.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * (c) Lucas van Staden <sales@proxiblue.com.au>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 class ProxiBlue_UpdateDownloadLinks_Model_Observer {
 
     public function catalog_product_save_after($observer) {
